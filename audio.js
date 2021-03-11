0642fdd02e288b910c776396cc4e3afb16ab0f53
Brad Czerniak <ao5357@gmail.com>
2021-03-11 10:24:49 -0500

    Add audio player that interacts with the file

--- audio.js
+++ audio.js
@@ -4,3 +4,17 @@
  */
 
 console.log('The audio.js script has loaded.');
+
+var fileInputs = document.querySelectorAll('input[type="file"]');
+
+Array.prototype.forEach.call(fileInputs, (elem) => {
+  elem.setAttribute('capture', '');
+  elem.insertAdjacentElement("beforeBegin", `
+    <audio id="${elem.name}-player" controls></audio>
+  `);
+  elem.addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    const url = URL.createObjectURL(file);
+    e.target.nextElementSibling.src = url;
+  });
+});
